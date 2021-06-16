ab56ca7f657edbda3bd1cd6a9c14d756502d426d
AnushaGiri417 <anushagiri417@gmail.com>
2021-06-16 10:32:43 -0400

    Planet2 is ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,9 @@ public class Main {
         Feron.Radius="823587325872385723895km/2";
         Feron.Temperature = "100000000000000000000000000f";
 
+        Planet2 Briha = new Planet2();
+        Briha.radius = " 34256647";
+        Briha.size = " very small";
+
     }
 }
